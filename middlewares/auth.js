a58dc5fc058dec1e459853bf441a74dd9b63b429
Achraf <achraf.rehioui.1@gmail.com>
2020-12-09 23:16:22 +0100

    Authorization [ is Admin User Middleware ]

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,11 +12,23 @@ exports.isAuth = (req, res, next) => {
 
     let user = req.profile && req.auth && (req.profile._id == req.auth._id)
 
-    if(!user) {
+    if (!user) {
         return res.status(403).json({
             error: "Access Denied"
         })
     }
 
     next()
+}
+
+exports.isAdmin = (req, res, next) => {
+
+    if (req.auth.role == 0) {
+        return res.status(403).json({
+            error: "Admin Resource, Access Denied !"
+        })
+    }
+
+    next();
+
 }
