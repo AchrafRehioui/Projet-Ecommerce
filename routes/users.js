e0cb4af4dbddde44ef86f2e064e016d4d5cba517
Achraf <achraf.rehioui.1@gmail.com>
2020-12-09 19:52:20 +0100

    Authorization [ Require Signin with Express-JWT]

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { hello, signup, signin, signout } = require('../controllers/userController');
 const { userSignUpValidator } = require('../middlewares/userValidator');
+const { requireSignIn } = require('../middlewares/auth');
 const router = express.Router();
 
 router.get('/', hello);
@@ -12,5 +13,8 @@ router.post('/signin', signin);
 
 router.get('/signout', signout);
 
+router.get('/hello', requireSignIn, (req, res) => {
+    res.send('hello there');
+})
 
 module.exports = router;
