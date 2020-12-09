1621d5b3246bb420fd6b51bf506f61ce018d536a
Achraf <achraf.rehioui.1@gmail.com>
2020-12-09 23:06:11 +0100

    Authorisation [ is Auth User Middleware ]

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,3 +7,16 @@ exports.requireSignIn = expressJWT({
     algorithms: ["HS256"],
     userProperty: 'auth'
 })
+
+exports.isAuth = (req, res, next) => {
+
+    let user = req.profile && req.auth && (req.profile._id == req.auth._id)
+
+    if(!user) {
+        return res.status(403).json({
+            error: "Access Denied"
+        })
+    }
+
+    next()
+}
