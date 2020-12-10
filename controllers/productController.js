8f245631c559237f39c6b9360d1fbf0f2cb980f4
Achraf <achraf.rehioui.1@gmail.com>
2020-12-10 13:46:19 +0100

    Show Single Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -66,3 +66,30 @@ exports.createProduct = (req, res) => {
 
     })
 }
+
+
+exports.productById = (req, res, next, id) => {
+
+    Product.findById(id).exec((err, product) => {
+
+        if (err || !product) {
+            return res.status(404).json({
+                error: 'Product not found !'
+            })
+        }
+
+        req.product = product;
+        next()
+
+    })
+
+}
+
+exports.showProduct = (req, res) => {
+
+    req.product.photo = undefined;
+
+    res.json({
+        product: req.product
+    })
+}
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
 const { userById } = require('../middlewares/user');
 
 
 const router = express.Router();
 
-const { createProduct } = require('../controllers/productController');
+const { createProduct, showProduct, productById } = require('../controllers/productController');
 
 const { requireSignIn, isAuth, isAdmin } = require('../middlewares/auth');
 
+router.get('/:productId', showProduct);
+
 router.post('/create/:userId', [requireSignIn, isAuth, isAdmin], createProduct);
 
-router.param('userId', userById)
+router.param('userId', userById);
+
+router.param('productId', productById);
 
 module.exports = router;
