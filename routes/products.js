14bcfb981fb9fb84bb773a6dc41cf331be939872
Achraf <achraf.rehioui.1@gmail.com>
2020-12-11 01:28:17 +0100

    Update Product

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const { userById } = require('../middlewares/user');
 
 const router = express.Router();
 
-const { createProduct, showProduct, productById, removeProduct } = require('../controllers/productController');
+const { createProduct, showProduct, productById, removeProduct, updateProduct } = require('../controllers/productController');
 
 const { requireSignIn, isAuth, isAdmin } = require('../middlewares/auth');
 
@@ -12,7 +12,9 @@ router.get('/:productId', showProduct);
 
 router.post('/create/:userId', [requireSignIn, isAuth, isAdmin], createProduct);
 
-router.delete('/:productId/:userId', [requireSignIn, isAuth, isAdmin], removeProduct)
+router.put('/:productId/:userId', [requireSignIn, isAuth, isAdmin], updateProduct)
+
+router.delete('/:productId/:userId', [requireSignIn, isAuth, isAdmin], removeProduct);
 
 router.param('userId', userById);
 
