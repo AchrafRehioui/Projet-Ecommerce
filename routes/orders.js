1cba450d988fd8faf9b4b750f9c844d581e4ae15
Achraf <achraf.rehioui.1@gmail.com>
2020-12-25 20:07:00 +0100

    adding management Stock Quantity

--- routes/orders.js
+++ routes/orders.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const { userById, addProductsToUserHistory } = require('../middlewares/user');
 
-const { requireSignIn, isAuth } = require('../middlewares/auth');
+const { decreaseQuantity } = require('./../middlewares/product');
 
+const { requireSignIn, isAuth } = require('../middlewares/auth');
 
 const { create } = require('./../controllers/OrderController');
 
 const router = express.Router();
 
-router.post('/create/:userId', [requireSignIn, isAuth, addProductsToUserHistory], create);
+router.post('/create/:userId', [requireSignIn, isAuth, addProductsToUserHistory, decreaseQuantity], create);
 
 router.param('userId', userById);
 
